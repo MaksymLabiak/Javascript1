cd9fc08bf11e27ede0ce98a36b8385dfebb126f5
Maksym <labjakmakcim98@gmail.com>
2024-07-14 22:19:27 +0300

    24/25 + online store

---
+++ NEW lesson 24/main.js
@@ -0,0 +1,20 @@
+// // localStorage - дані не видаляються після закриття браузера
+
+// localStorage.setItem("name", "Maksym");
+// localStorage.setItem("age", "25");
+// localStorage.getItem("name");
+// // localStorage.removeItem("name")
+
+// // sessionStorage - дані видаляються після закриття браузера
+
+// sessionStorage.setItem("name", "Maksym");
+// sessionStorage.setItem("age", "25");
+// sessionStorage.getItem("name");
+
+// // cockie - дані не видаляються після закриття браузера
+
+// document.cookie = "name=Maksym"
+// document.cookie = "age=25"
+// // document.cookie = "age=25; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+
+// Event loop / Цикл подій
